290883c973a8d7fe5206b1b5e251455dc1ffa810
ARFP <tedi@arfp.asso.fr>
2019-12-20 11:00:11 +0100

    Update Db.php

--- WebApp/Models/Db.php
+++ WebApp/Models/Db.php
@@ -11,22 +11,24 @@ class Db
 
     public static function getDb()
     {
-
-        if(Db::$instance === null)
-        {
-            $options = [
-                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
-                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-                \PDO::ATTR_EMULATE_PREPARES => false
-            ];
-            
-            Db::$instance = new \PDO(
-                'mysql:host=localhost;port=3306;dbname=flipquizz;charset=utf8',
-                'root', 
-                '', 
-                $options
-            );
-
+        if(Db::$instance === null) {
+            try {
+                $options = [
+                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+                    \PDO::ATTR_EMULATE_PREPARES => false
+                ];
+                
+                Db::$instance = new \PDO(
+                    'mysql:host=localhost;port=3306;dbname=flipquizz;charset=utf8',
+                    'root', 
+                    '', 
+                    $options
+                );
+            }
+            catch(\PDOException $e) {
+                exit('Error Connecting Database');
+            }
         }
 
         return Db::$instance;
