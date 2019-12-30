bbfde2b372144e456ece8c9cb28cf90aa6b7d504
MDevoldere <mdevoldere@devoldere.net>
2019-12-30 23:03:35 +0100

    v0.3.3 fix

---
+++ demo/resources/resources.php
@@ -0,0 +1,27 @@
+<?php
+$id = !empty($_GET['id']) ? basename($_GET['id']) : 'css';
+
+switch($id) 
+{
+    case 'css': 
+        header('Content-Type: text/css;charset=UTF-8');
+        $css = '';
+        foreach(glob(__DIR__.'/*.png') as $k => $v) {
+            $c = basename($v, '.png');
+            $n = basename($v);
+            $css .= ".".$c.":before { background-image: url('./".$n."');}\n";
+        }
+        exit($css);
+    break;
+    case 'js':
+        header('Content-Type: application/javascript');
+        $js = [];
+        foreach(glob(__DIR__.'/*.png') as $k => $v) {
+            $c = basename($v, '.png');
+            $js[] = "'".$c."'";
+        }
+        exit('const avatars = ['.implode(',', $js).']');
+    break;
+    default:
+    break;
+}
