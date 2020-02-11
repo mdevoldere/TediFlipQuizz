6ad15831d11aee5d9517c0fcd05e7863d81e7108
ARFP <tedi@arfp.asso.fr>
2020-02-11 08:23:59 +0100

    Consignes catégories/questions

--- WebApp/www/manage/categories.php
+++ WebApp/www/manage/categories.php
@@ -3,5 +3,21 @@
 
 <h3>List of categories</h3>
 
+<?php
+// Lister les catégories existantes.
+// Pour chaque catégorie, afficher le nom du quiz (requête SQL avec jointure)
+?>
 
 <h3>Add new category</h3>
+
+<?php
+// Implémenter le formulaire d'ajout de catégorie
+// Le formulaire doit afficher une liste déroulante permettant de lier la catégorie à un quiz existant.
+// Boucler sur les quzi existants pour créer chaque option du select
+// <select name="quiz_id">
+// foreach(quizzes) 
+// <option value="{quiz_id}">{quiz_theme}</option>
+// fin foreach
+// </select>
+// Une fois le formulaire implémenté, coder la page de traitement.
+?>
--- WebApp/www/manage/questions.php
+++ WebApp/www/manage/questions.php
@@ -3,4 +3,21 @@
 
 <h3>List of questions with their associated answer</h3>
 
+<?php
+// Lister les 20 dernières questions ajoutées.
+// Pour chaque question, afficher le nom de la catégorie et le nom du quiz (requête SQL avec 2 jointures)
+?>
+
 <h3>Add new question</h3>
+
+<?php
+// Implémenter le formulaire d'ajout de question
+// Le formulaire doit afficher une liste déroulante permettant de lier la question à une catégorie existante.
+// Boucler sur les catégories existantes pour créer chaque option du select
+// <select name="category_id">
+// foreach(categories) 
+// <option value="{category_id}">{quiz_theme} - {category_name}</option>
+// fin foreach
+// </select>
+// Une fois le formulaire implémenté, coder la page de traitement.
+?>
